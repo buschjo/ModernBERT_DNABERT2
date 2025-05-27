01c52d85ac47f0f1155baffe91b4f760fe78dd2e
s0560106 <s0560106@htw-berlin.de>
2025-05-27 16:22:09 +0200

    finetuning code and environment description

---
+++ dnabert2/finetuning/dnabert2_finetuning.py
@@ -0,0 +1,285 @@
+# adapted from https://github.com/MAGICS-LAB/DNABERT_2/blob/main/finetune/train.py (accessed: 18.05.2025)
+
+import os
+import csv
+import copy
+import json
+import logging
+from dataclasses import dataclass, field
+from typing import Any, Optional, Dict, Sequence, Tuple, List, Union
+
+import torch
+import transformers
+import sklearn
+import numpy as np
+from torch.utils.data import Dataset
+
+import helix_swap
+
+os.environ["WANDB_PROJECT"] = "ma_finetuning"
+
+### adapted by Josefine Busch
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default="zhihan1996/DNABERT-2-117M")
+    tokenizer_path: Optional[str] = field(default="zhihan1996/DNABERT-2-117M")
+### end of adapted by Josefine Busch
+
+@dataclass
+class DataArguments:
+    data_path: str = field(default=None, metadata={"help": "Path to the training data."})
+    kmer: int = field(default=-1, metadata={"help": "k-mer for input sequence. -1 means not using k-mer."})
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    optim: str = field(default="adamw_torch")
+    model_max_length: int = field(default=512, metadata={"help": "Maximum sequence length."})
+    gradient_accumulation_steps: int = field(default=1)
+    per_device_train_batch_size: int = field(default=1)
+    per_device_eval_batch_size: int = field(default=1)
+    num_train_epochs: int = field(default=1)
+    fp16: bool = field(default=False)
+    logging_steps: int = field(default=100)
+    save_steps: int = field(default=100)
+    eval_steps: int = field(default=100)
+    eval_strategy: str = field(default="epoch")
+    save_strategy: str = field(default="epoch")
+    logging_strategy: str = field(default="epoch")
+    warmup_steps: int = field(default=50)
+    weight_decay: float = field(default=0.01)
+    learning_rate: float = field(default=1e-4)
+    save_total_limit: int = field(default=3)
+    load_best_model_at_end: bool = field(default=True)
+    output_dir: str = field(default="output")
+    find_unused_parameters: bool = field(default=False)
+    checkpointing: bool = field(default=False)
+    dataloader_pin_memory: bool = field(default=False)
+    eval_and_save_results: bool = field(default=True)
+    save_model: bool = field(default=True)
+    logging_dir: str = 'logs'
+    seed: int = field(default=42)
+    report_to="wandb"
+    run_name: str = field(default="run")
+
+def safe_save_model_for_hf_trainer(trainer: transformers.Trainer, output_dir: str):
+    """Collects the state dict and dump to disk."""
+    state_dict = trainer.model.state_dict()
+    if trainer.args.should_save:
+        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
+        del state_dict
+        trainer._save(output_dir, state_dict=cpu_state_dict)  # noqa
+
+"""
+Transform a dna sequence to k-mer string
+"""
+def generate_kmer_str(sequence: str, k: int) -> str:
+    """Generate k-mer string from DNA sequence."""
+    return " ".join([sequence[i:i+k] for i in range(len(sequence) - k + 1)])
+
+"""
+Load or generate k-mer string for each DNA sequence. The generated k-mer string will be saved to the same directory as the original data with the same name but with a suffix of "_{k}mer".
+"""
+def load_or_generate_kmer(data_path: str, texts: List[str], k: int) -> List[str]:
+    """Load or generate k-mer string for each DNA sequence."""
+    kmer_path = data_path.replace(".csv", f"_{k}mer.json")
+    if os.path.exists(kmer_path):
+        logging.warning(f"Loading k-mer from {kmer_path}...")
+        with open(kmer_path, "r") as f:
+            kmer = json.load(f)
+    else:        
+        logging.warning(f"Generating k-mer...")
+        kmer = [generate_kmer_str(text, k) for text in texts]
+        with open(kmer_path, "w") as f:
+            logging.warning(f"Saving k-mer to {kmer_path}...")
+            json.dump(kmer, f)
+        
+    return kmer
+
+class SupervisedDataset(Dataset):
+    """Dataset for supervised fine-tuning."""
+
+    def __init__(self, 
+                 data_path: str, 
+                 tokenizer: transformers.PreTrainedTokenizer, 
+                 kmer: int = -1):
+
+        super(SupervisedDataset, self).__init__()
+
+        # load data from the disk
+        with open(data_path, "r") as f:
+            data = list(csv.reader(f))[1:]
+        if len(data[0]) == 2:
+            # data is in the format of [text, label]
+            logging.warning("Perform single sequence classification...")
+            texts = [d[0] for d in data]
+            labels = [int(d[1]) for d in data]
+        elif len(data[0]) == 3:
+            # data is in the format of [text1, text2, label]
+            logging.warning("Perform sequence-pair classification...")
+            texts = [[d[0], d[1]] for d in data]
+            labels = [int(d[2]) for d in data]
+        else:
+            raise ValueError("Data format not supported.")
+        
+        if kmer != -1:
+            # only write file on the first process
+            if torch.distributed.get_rank() not in [0, -1]:
+                torch.distributed.barrier()
+
+            logging.warning(f"Using {kmer}-mer as input...")
+            texts = load_or_generate_kmer(data_path, texts, kmer)
+
+            if torch.distributed.get_rank() == 0:
+                torch.distributed.barrier()
+
+        output = tokenizer(
+            texts,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+        )
+
+        self.input_ids = output["input_ids"]
+        self.attention_mask = output["attention_mask"]
+        self.labels = labels
+        self.num_labels = len(set(labels))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return dict(input_ids=self.input_ids[i], labels=self.labels[i])
+
+
+@dataclass
+class DataCollatorForSupervisedDataset(object):
+    """Collate examples for supervised fine-tuning."""
+
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.Tensor(labels).long()
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
+
+"""
+Manually calculate the accuracy, f1, matthews_correlation, precision, recall with sklearn.
+"""
+def calculate_metric_with_sklearn(predictions: np.ndarray, labels: np.ndarray):
+    valid_mask = labels != -100  # Exclude padding tokens (assuming -100 is the padding token ID)
+    valid_predictions = predictions[valid_mask]
+    valid_labels = labels[valid_mask]
+    return {
+        "accuracy": sklearn.metrics.accuracy_score(valid_labels, valid_predictions),
+        "f1": sklearn.metrics.f1_score(
+            valid_labels, valid_predictions, average="macro", zero_division=0
+        ),
+        "matthews_correlation": sklearn.metrics.matthews_corrcoef(
+            valid_labels, valid_predictions
+        ),
+        "precision": sklearn.metrics.precision_score(
+            valid_labels, valid_predictions, average="macro", zero_division=0
+        ),
+        "recall": sklearn.metrics.recall_score(
+            valid_labels, valid_predictions, average="macro", zero_division=0
+        ),
+    }
+
+# from: https://discuss.huggingface.co/t/cuda-out-of-memory-when-using-trainer-with-compute-metrics/2941/13
+def preprocess_logits_for_metrics(logits:Union[torch.Tensor, Tuple[torch.Tensor, Any]], _):
+    if isinstance(logits, tuple):  # Unpack logits if it's a tuple
+        logits = logits[0]
+
+    if logits.ndim == 3:
+        # Reshape logits to 2D if needed
+        logits = logits.reshape(-1, logits.shape[-1])
+
+    return torch.argmax(logits, dim=-1)
+
+
+"""
+Compute metrics used for huggingface trainer.
+""" 
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    return calculate_metric_with_sklearn(predictions, labels)
+
+
+
+def train():
+    parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
+    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+	### adapted by Josefine Busch
+    # load tokenizer
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        # model_args.model_name_or_path,
+        model_args.tokenizer_path,
+        cache_dir=training_args.cache_dir,
+        model_max_length=training_args.model_max_length,
+        padding_side="right",
+        use_fast=True,
+        trust_remote_code=True,
+    )
+    ### end of adapted by Josefine Busch
+
+    # define datasets and data collator
+    train_dataset = SupervisedDataset(tokenizer=tokenizer, 
+                                      data_path=os.path.join(data_args.data_path, "train.csv"),
+                                      kmer=data_args.kmer)
+    val_dataset = SupervisedDataset(tokenizer=tokenizer, 
+                                     data_path=os.path.join(data_args.data_path, "dev.csv"),
+                                     kmer=data_args.kmer)
+    test_dataset = SupervisedDataset(tokenizer=tokenizer, 
+                                     data_path=os.path.join(data_args.data_path, "test.csv"),
+                                     kmer=data_args.kmer)
+    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+
+
+    # load model
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        num_labels=train_dataset.num_labels,
+        trust_remote_code=True,
+    )
+
+    # define trainer
+    print('load model...')
+    trainer = transformers.Trainer(model=model,
+                                   tokenizer=tokenizer,
+                                   args=training_args,
+                                   preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+                                   compute_metrics=compute_metrics,
+                                   train_dataset=train_dataset,
+                                   eval_dataset=val_dataset,
+                                   data_collator=data_collator)
+    model.train()
+    print('start training...')
+    trainer.train()
+
+    if training_args.save_model:
+        trainer.save_state()
+        safe_save_model_for_hf_trainer(trainer=trainer, output_dir=training_args.output_dir)
+        model.save_pretrained(f'{training_args.output_dir}/{training_args.run_name}_final_model')
+
+    # get the evaluation results from trainer
+    model.eval()
+    if training_args.eval_and_save_results:
+        results_path = os.path.join(training_args.output_dir, "results", training_args.run_name)
+        results = trainer.evaluate(eval_dataset=test_dataset, metric_key_prefix="test")
+        os.makedirs(results_path, exist_ok=True)
+        with open(os.path.join(results_path, "eval_results.json"), "w") as f:
+            json.dump(results, f)
+
+if __name__ == "__main__":
+    train()
---
+++ dnabert2/finetuning/run_prom_tasks.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# adapted from https://github.com/MAGICS-LAB/DNABERT_2/blob/main/finetune/scripts/run_dnabert2.sh (accessed: 01.05.2025)
+
+data_path=$1
+scratch=/scratch/jbusch/ma/
+# base_model="zhihan1996/DNABERT-2-117M"
+base_model=${scratch}models/modern_bert/converted_to_hf/hs_tokenizer5M_v5
+# tokenizer_path=${scratch}tokenizer/hf_tokenizer/hf_dnabert_sub5M
+# tokenizer_path=${scratch}models/modern_bert/converted_to_hf/test_conversion
+tokenizer_path=${scratch}tokenizer/hs_tokenizer/fixed_encode_decode/sub5M_tokenizer
+# tokenizer_path="zhihan1996/DNABERT-2-117M"
+output_path=${scratch}models/modern_bert/finetuning/
+cache_dir=${scratch}models/finetuning/cache/
+lr=3e-5
+
+echo "The provided data_path is $data_path"
+
+for seed in 42
+do
+    # for data in prom_core_all prom_core_notata
+    # do
+    #     python dnabert2_finetuning.py \
+    #         --model_name_or_path ${base_model} \
+    #         --data_path  $data_path/GUE/prom/$data \
+    #         --kmer -1 \
+    #         --run_name DNABERT2_${vocab}_${lr}_prom_${data}_seed${seed} \
+    #         --model_max_length 20 \
+    #         --per_device_train_batch_size 8 \
+    #         --per_device_eval_batch_size 16 \
+    #         --gradient_accumulation_steps 1 \
+    #         --learning_rate ${lr} \
+    #         --num_train_epochs 4 \
+    #         --fp16 \
+    #         --save_steps 400 \
+    #         --output_dir ${output_path}output/dnabert2 \
+	# 		--cache_dir ${cache_dir} \
+    #         --eval_strategy epoch \
+    #         --eval_steps 400 \
+    #         --warmup_steps 50 \
+    #         --logging_steps 100000 \
+    #         --overwrite_output_dir True \
+    #         --log_level info \
+	# 		--save_strategy epoch \
+    #         --find_unused_parameters False
+    # done
+
+    # for data in prom_core_tata
+    # do
+    #     python dnabert2_finetuning.py \
+    #         --model_name_or_path ${base_model} \
+    #         --data_path  $data_path/GUE/prom/$data \
+    #         --kmer -1 \
+    #         --run_name DNABERT2_${vocab}_${lr}_prom_${data}_seed${seed} \
+    #         --model_max_length 20 \
+    #         --per_device_train_batch_size 8 \
+    #         --per_device_eval_batch_size 16 \
+    #         --gradient_accumulation_steps 1 \
+    #         --learning_rate ${lr} \
+    #         --num_train_epochs 10 \
+    #         --fp16 \
+    #         --save_steps 200 \
+    #         --output_dir ${output_path}output/dnabert2 \
+	# 		--cache_dir ${cache_dir} \
+    #         --eval_strategy epoch \
+    #         --eval_steps 200 \
+    #         --warmup_steps 50 \
+    #         --logging_steps 100000 \
+    #         --overwrite_output_dir True \
+    #         --log_level info \
+	# 		--save_strategy epoch \
+    #         --find_unused_parameters False
+    # done
+
+    # for data in prom_300_all prom_300_notata
+    # do
+    #     python dnabert2_finetuning.py \
+    #         --model_name_or_path ${base_model} \
+    #         --data_path  $data_path/GUE/prom/$data \
+    #         --kmer -1 \
+    #         --run_name DNABERT2_${vocab}_${lr}_prom_${data}_seed${seed} \
+    #         --model_max_length 70 \
+    #         --per_device_train_batch_size 8 \
+    #         --per_device_eval_batch_size 16 \
+    #         --gradient_accumulation_steps 1 \
+    #         --learning_rate ${lr} \
+    #         --num_train_epochs 4 \
+    #         --fp16 \
+    #         --save_steps 400 \
+    #         --output_dir ${output_path}output/dnabert2 \
+	# 		--cache_dir ${cache_dir} \
+    #         --eval_strategy epoch \
+    #         --eval_steps 400 \
+    #         --warmup_steps 50 \
+    #         --logging_steps 100000 \
+    #         --overwrite_output_dir True \
+    #         --log_level info \
+	# 		--save_strategy epoch \
+    #         --find_unused_parameters False
+    # done
+
+    for data in prom_300_tata
+    do 
+        python dnabert2_finetuning.py \
+            --model_name_or_path ${base_model} \
+			--tokenizer_path ${tokenizer_path} \
+            --data_path  $data_path/GUE/prom/$data \
+            --kmer -1 \
+            --run_name DNABERT2_${vocab}_${lr}_prom_${data}_seed${seed} \
+            --model_max_length 70 \
+            --per_device_train_batch_size 8 \
+            --per_device_eval_batch_size 16 \
+            --gradient_accumulation_steps 1 \
+            --learning_rate ${lr} \
+            --num_train_epochs 10 \
+            --fp16 \
+            --save_steps 200 \
+            --output_dir ${output_path}output/dnabert2 \
+			--cache_dir ${cache_dir} \
+            --eval_strategy epoch \
+            --eval_steps 200 \
+            --warmup_steps 50 \
+            --logging_steps 100000 \
+            --overwrite_output_dir True \
+            --log_level info \
+			--save_strategy epoch \
+            --find_unused_parameters False
+    done 
+done
