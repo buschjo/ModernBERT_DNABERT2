1dfa0d49009014ad966b266ba1636f1c1fb60184
s0560106 <s0560106@htw-berlin.de>
2025-05-27 17:07:43 +0200

    cleanup

--- hs_tokenizer/test_cuda.py
+++
@@ -1,9 +0,0 @@
-print('DNA-BERT Container')
-
-from torch import cuda as tc
-x=tc.is_available()
-print('Is cuda is_available:', x)
-if x:
-    print(tc.get_device_name())
-else:
-    print('running on cpu...')
